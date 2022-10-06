5ea947f5fb9aef63ef2f07b8234a1e232554ae96
Gryuker <eskeleto@1958.outlook.es>
2022-10-06 00:26:57 -0300

    Agustin: Nuevas cosas en despedida.jsx

--- src/components/despedida.jsx
+++ src/components/despedida.jsx
@@ -4,12 +4,12 @@ function Despedida(){
     return(
          <>
          <div className='Parrafo'>
-            <h3 id='despedida-titulo'>Fundamentos de Programacion Web</h3>
-            <p id='despedida-texto1'>Muchas gracias por jugar nuestro juego.</p>
-            <ul id='despedida-texto2'>Los Pokemon te agradecen por jugar con ellos.</ul>
+            <h3 id='despedida-titulo'>Fundamentos de Programacion Web.</h3>
+            <p id='despedida-texto1'>Espereramos que les haya divertido nuestro juego.</p>
+            <ul id='despedida-texto2'>Muchas gracias por jugarlo.</ul>
         </div>
         <div className='Despedida'>
-                <img src='' alt="imagen despedida"></img>
+                <img src='/assets/images/Imagen Despedida.jpg' alt="Imagen Despedida"></img>
         </div></>
     )
 }
