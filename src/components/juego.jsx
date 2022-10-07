e103f176218dbb825fdf718fbaa87c8628a5c070
Conrado Velazquez <29915636@fi.unju.edu.ar>
2022-10-07 13:59:50 -0300

    Conrado: juego funcionando, faltan sonidos

--- src/components/juego.jsx
+++ src/components/juego.jsx
@@ -1,6 +1,6 @@
-import Button from "react-bootstrap/Button";
 import "./juego.css";
 import { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
 import Alfabeto from "../json/alfabeto.json";
 import Diccionario from "../json/diccionario.json";
 import Vidas from "../json/vidas.json";
@@ -15,6 +15,12 @@ const Juego = () => {
   const [contadorErrores, setContadorErrores] = useState(0);
   const [vidasRestantes, setVidasRestantes] = useState(Vidas[0].img);
   const [finJuego, setFinJuego] = useState(false);
+  /* Habilitar / Deshabilitar botones dependiendo si es fin de juego */
+  useEffect(() => {
+    Alfabeto.map((letra) => {
+      document.getElementById(letra).disabled = finJuego;
+    });
+  }, [finJuego]);
 
   /* Generar botones abecedario */
   const botones = () => {
@@ -42,14 +48,21 @@ const Juego = () => {
     setPista(Diccionario[indPalabra].pista);
     setPalabraEnmascarada(arrayPalabraEnmascarada);
     setTecladoDeshabilitado(false);
+    setContadorErrores(0);
+    setVidasRestantes(Vidas[0].img);
+    setFinJuego(false);
+    Alfabeto.map((letra) => {
+      document.getElementById(letra).disabled = false;
+    });
   };
 
   /* Añadir letra presionada a una variable */
   const presionarTecla = (letra) => {
     let banderaError = true;
     let contErrores = contadorErrores;
-    /* Deshabilitar tecla presionada */
-    let palabraActualizada = palabraEnmascarada.map(l => l);
+    /* construyo una variable con el contenido de palabraEnmascarada para poder actualizar su estado más adelante */
+    let palabraActualizada = palabraEnmascarada.map((l) => l);
+    /* Deshabilitar teclas presionadas */
     document.getElementById(letra).disabled = true;
 
     /* Controlar si la letra está en la palabra y mostramos todas sus ocurrencias */
@@ -64,59 +77,60 @@ const Juego = () => {
       }
     }
 
-    /* Si la letra no está en la palabra incrementar el contador de errores y actualizar imágen ahorcado */
+    /* Controlar si la letra no está en la palabra e incrementar el contador de errores en ese caso*/
     if (banderaError) {
       contErrores++;
-      console.log(contErrores," - ",Vidas.length);
-      if (contErrores <= (Vidas.length-1)){
+      /* Mientras tenga vidas disponibles, incrementeo el contador de errores y actualizo la imagen del ahoracado */
+      if (contErrores <= Vidas.length - 1) {
         setContadorErrores(contErrores);
         setVidasRestantes(Vidas[contErrores].img);
-      }else{
+      } else {
+        /* Si no tiene vidas, pierde */
         setVidasRestantes("/assets/images/juego_perder.png");
         setFinJuego(true);
       }
     }
-  
+
     /* Controlar si el contenido de ambos arrays son iguales, para saber si ganó el jugador */
-    if (palabraActualizada.every((value, index) => value === palabraElegida[index])) {
-      //Deshabilitar teclado
-      //Mostrar imágen victoria
-      //sonido de victoria NICOLE
+    if (
+      palabraActualizada.every(
+        (value, index) => value === palabraElegida[index]
+      )
+    ) {
+      //sonido de victoria
       setVidasRestantes("/assets/images/juego_ganar.png");
       setFinJuego(true);
     }
   };
 
   return (
-    <>
-      <div className="full-game-area">
-        <section className="sup-game-area">
-          <article className="left-game-area">
-            <div className="pistas-area">
-              <p> {pista} </p>
-            </div>
-            <div className="palabra-area">
-              <p> {palabraEnmascarada} </p>
-            </div>
-          </article>
-          <article className="right-game-area">
-            <img src={vidasRestantes} alt="imágen ahorcado"></img>
-          </article>
-        </section>
-        <footer className="inf-game-area">
-          <div className="teclado"> {botones()} </div>
-          <div className="controles">
-            <Button
-              className="button-iniciar"
-              variant="secondary"
-              onClick={() => iniciarJugada()}
-            >
-              Obtener palabra
-            </Button>
+    <div className="full-game-area">
+      <section className="sup-game-area">
+        <article className="left-game-area">
+          <div className="pistas-area">
+            <p> {pista} </p>
           </div>
-        </footer>
-      </div>
-    </>
+          <div className="palabra-area">
+            <p> {palabraEnmascarada} </p>
+          </div>
+        </article>
+        <article className="right-game-area">
+          <img src={vidasRestantes} alt="imágen ahorcado"></img>
+        </article>
+      </section>
+      <footer className="inf-game-area">
+        <div className="teclado"> {botones()} </div>
+        <div className="controles">
+          <Button
+            className="button-iniciar"
+            variant="secondary"
+            onClick={() => iniciarJugada()}
+          >
+            Obtener palabra
+          </Button>
+        </div>
+      </footer>
+    </div>
   );
 };
 
