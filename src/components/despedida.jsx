50516652ffb17ea92e6b51727856bbabd48a1dce
Gryuker <eskeleto@1958.outlook.es>
2022-10-07 02:58:21 -0300

    Agustin: Agregacion de la pantalla Error404

--- src/components/despedida.jsx
+++ src/components/despedida.jsx
@@ -3,14 +3,15 @@ import "../components/despedida.css";
 function Despedida(){
     return(
          <>
-         <div className='Parrafo'>
+        <div className='Parrafo'>
             <h3 id='despedida-titulo'>Fundamentos de Programacion Web.</h3>
             <p id='despedida-texto1'>Espereramos que les haya divertido nuestro juego.</p>
             <ul id='despedida-texto2'>Muchas gracias por jugarlo.</ul>
         </div>
         <div className='Despedida'>
-                <img src='/assets/images/Imagen Despedida.jpg' alt="Imagen Despedida"></img>
+            <img src='/assets/images/Imagen Despedida.jpg' alt="Imagen Despedida"></img>
         </div></>
     )
 }
+
 export default Despedida;
